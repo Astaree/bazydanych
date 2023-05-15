4d9874a5c25fa383e88eaf03ad56476e1fe896c7
Astaree <56613649+Astaree@users.noreply.github.com>
2023-05-15 09:59:02 +0200

    api upd

--- realacyjna/server/app.py
+++ realacyjna/server/app.py
@@ -13,29 +13,35 @@ from resources.dormitory import Dormitory, DormitoryList
 app = Flask(__name__)
 api = Api(app)
 
+# Db routes
 # Add routes for the university resource
-api.add_resource(University, '/universities/<int:id>')
-api.add_resource(UniversityList, '/universities')
+api.add_resource(University, '/api/universities/<int:id>')
+api.add_resource(UniversityList, '/api/universities')
 
 # Add routes for the Major resource
-api.add_resource(Major, '/major/<int:id>')
-api.add_resource(MajorList, '/major')
+api.add_resource(Major, '/api/major/<int:id>')
+api.add_resource(MajorList, '/api/major')
 
 # Add routes for the Student resource
-api.add_resource(Student, '/students/<int:id>')
-api.add_resource(StudentList, '/students')
+api.add_resource(Student, '/api/students/<int:id>')
+api.add_resource(StudentList, '/api/students')
 
 # Add routes for the Student in dormitory resource
-api.add_resource(StudentDorm, '/students_dormitory/<int:id>')
-api.add_resource(StudentDormList, '/students_dormitory')
+api.add_resource(StudentDorm, '/api/students_dormitory/<int:id>')
+api.add_resource(StudentDormList, '/api/students_dormitory')
 
 # Add routes for the Student in major resource
-api.add_resource(StudentMajor, '/students_major/<int:id>')
-api.add_resource(StudentMajorList, '/students_major')
+api.add_resource(StudentMajor, '/api/students_major/<int:id>')
+api.add_resource(StudentMajorList, '/api/students_major')
 
 # Add routes for the Dormitory resource
-api.add_resource(Dormitory, '/dormitories/<int:id>')
-api.add_resource(DormitoryList, '/dormitories')
+api.add_resource(Dormitory, '/api/dormitories/<int:id>')
+api.add_resource(DormitoryList, '/api/dormitories')
+
+
+# client routes
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
